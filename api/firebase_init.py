464b365a902a968e59162dbcc5725a36aef01962
Khalef <Khalef@Khalefs-MacBook-Pro.local>
2025-06-23 16:58:43 +0000

    fixing firebase config

--- api/firebase_init.py
+++ api/firebase_init.py
@@ -1,8 +1,9 @@
 import os
-from firebase_admin import credentials, initialize_app
+import firebase_admin
+from firebase_admin import credentials
 
 cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
-if not initialize_app._apps:
+if not firebase_admin._apps:
     cred = credentials.Certificate(cred_path)
-    initialize_app(cred)
+    firebase_admin.initialize_app(cred)
