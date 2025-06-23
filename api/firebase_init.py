871969f07e87eb951a3b405d162aa969e721a646
Khalef <Khalef@Khalefs-MacBook-Pro.local>
2025-06-23 16:33:00 +0000

    Hiding firebase config

--- api/firebase_init.py
+++ api/firebase_init.py
@@ -1,5 +1,9 @@
+from decouple import config
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("firebase_admin_sdk.json")
-firebase_admin.initialize_app(cred)
+cred_path = config("GOOGLE_APPLICATION_CREDENTIALS")
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(cred_path)
+    firebase_admin.initialize_app(cred)
